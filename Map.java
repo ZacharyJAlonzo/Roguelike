3dd2659c5d3347825ab465bff5d67410522e63c4
ZacharyJAlonzo <62187352+ZacharyJAlonzo@users.noreply.github.com>
2020-03-14 14:46:48 -0500

    Add files via upload

---
+++ Map.java
@@ -0,0 +1,28 @@
+/**
+ * @(#)Map.java
+ *
+ *
+ * @author 
+ * @version 1.00 2020/2/23
+ */
+import java.awt.*;
+import java.awt.event.*;
+
+public class Map 
+{
+
+   
+   public static void main(String [] args)
+   {
+   
+   	 Tile x = new Tile(50);
+   	 
+   	 System.out.println();
+   	 
+   	 MapDisplay y = new MapDisplay(x);
+ 	 
+   	 
+   	 
+   }
+    
+}
